18162979c9dc0e05a03b710af4ad6bcbf4959400
marcogarz <dev.84ui7@aleeas.com>
2022-10-13 16:48:57 -0500

    Note controller added

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        $notes = Note::where('user_id', Auth::user()->id)->get();
+
+        return view('dashboard', compact('notes'));
+    }
+}

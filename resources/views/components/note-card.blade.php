8c829964b2541dcad53b3a9a5f2ec57e973da6c4
marcogarz <dev.84ui7@aleeas.com>
2022-10-14 17:16:11 -0500

    Animation added to cards

--- resources/views/components/note-card.blade.php
+++ resources/views/components/note-card.blade.php
@@ -1,6 +1,6 @@
 @props(['note'])
 
-<a href="{{ route('notes.show', $note) }}">
+<a href="{{ route('notes.show', $note) }}" class="hover:-translate-y-2 transition ease-in-out duration-100">
     <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm h-fit">
         <h5 class="text-gray-900 text-xl leading-tigth font-bold mb-2 break-words">{{ $note->title }}</h5>
         <p class="text-gray-400 text-sm mb-4 break-words">{{ $note->updated_at->diffForHumans() }}</p>

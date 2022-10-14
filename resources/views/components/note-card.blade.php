ce1da6dcf9a738e0fb6e1272b8622d90fef966d1
marcogarz <dev.84ui7@aleeas.com>
2022-10-14 09:49:53 -0500

    Design on card componen improved

--- resources/views/components/note-card.blade.php
+++ resources/views/components/note-card.blade.php
@@ -1,6 +1,6 @@
 @props(['note'])
 
-<div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+<div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm h-fit">
     <h5 class="text-gray-900 text-xl leading-tigth font-medium mb-2">{{ $note->title }}</h5>
     <p class="text-gray-700 text-base mb-4 break-words">{!! Str::limit(nl2br($note->description), 100) !!}</p>
     <a href="{{ route('notes.show', $note) }}">
